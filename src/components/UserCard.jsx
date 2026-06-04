991e9e8577318515cb8e7f9e19c059e33c828c9c
josemcnieto <josemcnieto@gmail.com>
2026-06-04 21:39:46 +0200

    tercero

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,19 +1,54 @@
-import './UserCard.css'
-import iconoLinkedIn from '../assets/linkedin.png'
+
+
+
+
+
+import linkedinLogo from "./linkedin.png";
+
 function UserCard(props) {
   return (
-    <div className='tarjeta'>
-        <img src={props.imagen} alt="Foto de perfil" className='tarjeta-img' />
-        <div className='tarjeta-info'>
-            <h3>{props.nombre}</h3>
-            <p className='profesion'>
-                {props.profesion}
-                <img src={iconoLinkedIn} alt="Logo Linkedin" className='icono-red-social'/> 
-            </p>
-            <button className='btn-conectar'>Conectar</button>
-        </div>
+    <div
+      className="bg-white w-64 rounded-xl shadow-lg 
+      overflow-hidden hover:shadow-2xl hover:-translate-y-2 transition-all
+      duration-300"
+    >
+      <img
+        src={props.imagen || "https://via.placeholder.com/400x300?text=Sin+Imagen"}
+        alt="Foto de Perfil"
+        className="w-full h-48 object-cover"
+      />
+
+      <div className="p-6 text-center">
+        <h3 className="text-xl font-bold text-gray-800">
+          {props.nombre || "Sin nombre"}
+        </h3>
+
+        <p className="text-sm text-gray-500 my-3 flex items-center justify-center gap-2">
+          {props.profesion || "Sin profesión"}
+        </p>
+
+        <button className="bg-blue-600 text-white px-6 py-2 rounded-full font-bold hover:bg-blue-700 transition-colors flex items-center justify-center gap-2 mx-auto">
+          <img src={linkedinLogo} alt="LinkedIn" className="w-5 h-5" />
+          Conectar
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserCard
+export default UserCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
